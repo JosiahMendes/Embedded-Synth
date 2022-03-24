0f578a7015f3457c8a76595e9d2c12071c3c367e
Ragviswa <rv519@ic.ac.uk>
2022-03-24 19:41:59 +0000

    Minor Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -54,7 +54,6 @@ const int HKOE_BIT = 6;
 
 // This is for 44kHz!!
 const int32_t stepSizes [] = {51076057, 54113197, 57330935, 60740010, 64351799, 68178356, 72232452, 76527617, 81078186, 85899346, 91007187, 96418756, };
-// const int32_t sine_factor[] = {132, 125, 118, 111, 105, 99, 93, 88, 83, 79, 74, 70};
 const int32_t sine_acc[] = {49, 52, 55, 58, 61, 65, 69, 73, 77, 82, 87, 92};
 
 const int16_t lookup_size = 2048;
@@ -70,9 +69,7 @@ const int32_t half_max = 1073741824;
 const uint8_t n = 4;
 
 volatile int32_t currentStepSize[n];
-// volatile int32_t currentSineFactor[n];
 volatile int32_t currentSineAcc[n];
-// volatile int32_t currentSineFactor2[n];
 
 
 // Knob
@@ -140,61 +137,29 @@ void findKeywithFunc(void (*func)(notes*)) {
       bool current_key = false;
       notes current_note = None;
       
+      int j = 0;
       for (int i=0; i<12; i++) {
         current_key = bool_array[i];
-        if (current_key) {current_note = ((notes)i);}
-        if (current_note != None) {
-          if (localNotesPressed[0] == None) {
-            localNotesPressed[0] = current_note;
-          } else if (localNotesPressed[1] == None){
-            localNotesPressed[1] = current_note;
-          } else if (localNotesPressed[2] == None){
-            localNotesPressed[2] = current_note;
-          } else if (localNotesPressed[3] == None){
-            localNotesPressed[3] = current_note;
-          } 
-          /* else if (localNotesPressed[4] == None){
-            localNotesPressed[4] = current_note;
-          } else if (localNotesPressed[5] == None){
-            localNotesPressed[5] = current_note;
-          } else if (localNotesPressed[6] == None){
-            localNotesPressed[6] = current_note;
-          } else if (localNotesPressed[7] == None){
-            localNotesPressed[7] = current_note;
-          } else if (localNotesPressed[8] == None){
-            localNotesPressed[8] = current_note;
-          } else if (localNotesPressed[9] == None){
-            localNotesPressed[9] = current_note;
-          } else {
-            //ignore for now
-          }
-          */
+        current_note = ((notes)i);
+        if (current_key && localNotesPressed[j] == None && j < n) {
+          localNotesPressed[j] = current_note;
+          j++;
         }
-        current_note = None;
       }
 
       func(localNotesPressed);
-
-      //func(localNotesPressed[0], localNotesPressed[1]);
-      // __atomic_store_n(&localNotesPressed,notesPressed,__ATOMIC_RELAXED);
 }
 
 void setStepSize(notes* note_list) {
 
       int32_t localCurrentStepSize;
-       // double localCurrentSineFactor2;
-      // int32_t localCurrentSineFactor;
       int32_t localCurrentSineAcc;
       for (int i=0; i<n; i++) {
         if (note_list[i] != None) {
           localCurrentStepSize = stepSizes[note_list[i]];
-          // localCurrentSineFactor2 = sine_factor_dec[note_list[i]];
-          // localCurrentSineFactor = sine_factor[note_list[i]];
           localCurrentSineAcc = sine_acc[note_list[i]];
           xSemaphoreTake(stepSizeMutex, portMAX_DELAY);
           currentStepSize[i] = localCurrentStepSize;
-          // currentSineFactor2[i] = localCurrentSineFactor2;
-          // currentSineFactor[i] = localCurrentSineFactor;
           currentSineAcc[i] = localCurrentSineAcc;
           xSemaphoreGive(stepSizeMutex);
         } else {
@@ -315,8 +280,6 @@ void sampleISR(){
       localCurrentSineAcc[i] = currentSineAcc[i];
     }
     for (int i=0; i<n; i++) {
-      //phaseAcc_sel[i] = int32_max*sin(localCurrentSineFactor2[i]*phaseAcc[i]);
-      //acc[i] += 512*localCurrentStepSize[i]/int32_max;
       acc[i] += localCurrentSineAcc[i];
       if (acc[i] > lookup_size) {
         acc[i] = 0;
@@ -367,19 +330,6 @@ void sampleISR(){
   int32_t Vout = phaseAcc_final >> 24;
   Vout = Vout >> (8 - knob3.get_rotation()/2); // Volume Control
   analogWrite(OUTR_PIN, Vout+128);
- 
-  /*
-  } else if (knob0.get_rotation() == 6 || knob0.get_rotation() == 7) {
-    phaseAcc_0_sel = sin(localCurrentSineFactor[0]*acc);
-    acc += 1;
-    phaseAcc_0_sel = int32_max*sin(3.14*int32_max/localCurrentStepSize[0]*acc_0);
-    if (acc_0 >= int32_max/localCurrentStepSize[0]) {
-      acc_0 = 0;
-    } else {
-      acc_0 += 1;
-    }
-  }
-  */
 }
 
 void setup() {
