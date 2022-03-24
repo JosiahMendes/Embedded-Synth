779ffca0bd7c62c41a0a3fb89e28b2667e3f5acf
HyunjoonJeon <hj1119@ic.ac.uk>
2022-03-24 16:30:48 +0000

    tried to rewrite scanKeysTask and decodeTask but not working

--- src/main.cpp
+++ src/main.cpp
@@ -72,6 +72,7 @@ volatile int32_t currentAverage[n];
 
 // global variable determining mode
 bool receiver = true;
+bool only_module = false;
 bool position_set = false; // indicates whether position has been set
 uint8_t position = 6; // undefined
 uint8_t octave = 7; // some initial value
@@ -129,7 +130,7 @@ void setRow(uint8_t rowIdx){
       digitalWrite(REN_PIN,HIGH);
 }
 
-void findKeywithFunc(void (*func)(notes*)) {
+void findKeyWithFunc(void (*func)(notes*)) {
       xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
       uint8_t CDs = keyArray[0];
       uint8_t EG = keyArray[1];
@@ -199,7 +200,7 @@ void setStepSize(notes* note_list) {
       int32_t localCurrentAverage;
       for (int i=0; i<n; i++) {
         if (note_list[i] != None) {
-          localCurrentStepSize = stepSizes[note_list[i]] >> (8 - octave);
+          localCurrentStepSize = stepSizes[note_list[i]] >> (5 - octave);
           localCurrentAverage = averages[note_list[i]];
           xSemaphoreTake(stepSizeMutex, portMAX_DELAY);
           currentStepSize[i] = localCurrentStepSize;
@@ -280,15 +281,24 @@ void setNoteName(notes* note_list) {
 }
 
 void setCommMessage(notes* note_list){
-  if(note_list[0] == None){
-      TX_Message[0] = 'R';
-      xQueueSend(msgOutQ, TX_Message, portMAX_DELAY);
-  } else {
-      TX_Message[0] = 'P';
-      TX_Message[1] = octave;
-      TX_Message[2] = note_list[0];
-      xQueueSend(msgOutQ, TX_Message, portMAX_DELAY);
-  }
+
+  TX_Message[0] = keyArray[0];
+  TX_Message[1] = keyArray[1];
+  TX_Message[2] = keyArray[2];
+  TX_Message[3] = octave;
+  TX_Message[4] = position;
+  xQueueSend(msgOutQ, TX_Message, portMAX_DELAY);
+
+
+  // if(note_list[0] == None){
+  //     TX_Message[0] = 'R';
+  //     xQueueSend(msgOutQ, TX_Message, portMAX_DELAY);
+  // } else {
+  //     TX_Message[0] = 'P';
+  //     TX_Message[1] = octave;
+  //     TX_Message[2] = note_list[0];
+  //     xQueueSend(msgOutQ, TX_Message, portMAX_DELAY);
+  // }
 }
 
 
@@ -307,8 +317,13 @@ void scanKeysTask(void * pvParameters) {
         keyArray[i] = readCols();
         xSemaphoreGive(keyArrayMutex);
     }
-    // Call function for setting stepsize
-    findKeywithFunc(&setCommMessage);
+
+    if(!receiver) {
+      findKeyWithFunc(&setCommMessage); // send keyArray to receiver
+    }
+    else if(receiver && only_module) { // receiver and it's the only module
+      findKeyWithFunc(&setStepSize);
+    }
 
     // Find rotation of knob, protected with a key array mutex?
     xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
@@ -331,7 +346,7 @@ void displayUpdateTask(void * pvParameters){
     vTaskDelayUntil( &xLastWakeTime, xFrequency );
 
     // Call function for setting notename
-    findKeywithFunc(&setNoteName);
+    findKeyWithFunc(&setNoteName);
 
     //Toggle LED
     digitalToggle(LED_BUILTIN);
@@ -364,87 +379,252 @@ void broadcastEndOfHandshake() {
   xQueueSend(msgOutQ, TX_Message, portMAX_DELAY);
 }
 
-void decodeTask(void * pvParameters) {
+void handshakeDecodeTask(void * pvParameters) {
   uint8_t local_RX_Message[8] = {0};
-  bool local_externalKeyPressed;
-  while (true) {
-    for (int i = 0; i < 3; i++) {
-      setRow(i);
-      delayMicroseconds(2);
-      keyArray[i] = readCols();
-    }
-    // Call function for setting stepsize
-    if(receiver && !externalKeyPressed)
-      findKeywithFunc(&setStepSize);
-    if(!receiver)
-      findKeywithFunc(&setCommMessage);
+  xQueueReceive(msgInQ, local_RX_Message, portMAX_DELAY);
+  //xSemaphoreTake(RX_Message_Mutex, portMAX_DELAY);
+  std::copy(std::begin(local_RX_Message), std::end(local_RX_Message), std::begin(RX_Message));
+  //xSemaphoreGive(RX_Message_Mutex);
 
-    // wait until a message is available in the queue
-    xQueueReceive(msgInQ, local_RX_Message, portMAX_DELAY);
-
-    xSemaphoreTake(RX_Message_Mutex, portMAX_DELAY);
-    std::copy(std::begin(local_RX_Message), std::end(local_RX_Message), std::begin(RX_Message));
-    xSemaphoreGive(RX_Message_Mutex);
-
-    switch(RX_Message[0]) {
-      case 'R':
-        {
-          local_externalKeyPressed = false;
-          __atomic_store_n(&externalKeyPressed,local_externalKeyPressed,__ATOMIC_RELAXED);
-          notes empty [4]= {None, None, None, None};
-          setStepSize(empty);
-          break;
-        }
-      case 'P':
-        {
-          local_externalKeyPressed = true;
-          __atomic_store_n(&externalKeyPressed,local_externalKeyPressed,__ATOMIC_RELAXED);
-          // TODO: Fixing this
-          int32_t localCurrentStepSize = stepSizes[RX_Message[2]] >> (5 - RX_Message[1]);
-          xSemaphoreTake(stepSizeMutex, portMAX_DELAY);
-          for (int i=0; i<n; i++) {
-            if(currentStepSize[i] == 0) {
-              currentStepSize[i] = localCurrentStepSize;
-              break;
-            };
+  switch(RX_Message[0]) {
+    case 'H':
+      {
+        xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
+        setRow(5);
+        delayMicroseconds(2);
+        keyArray[5] = readCols();
+        if(keyArray[5] >> 3 & B1) { // west turned off
+          if(!position_set) {
+            position = RX_Message[2] + 1;
+            position_set = true;
           }
-          xSemaphoreGive(stepSizeMutex);
-          break;
-        }
-      case 'H':
-        {
-          setRow(5);
-          delayMicroseconds(2);
-          keyArray[5] = readCols();
-          if(keyArray[5] >> 3 & B1) { // west turned off
-            if(!position_set) {
-              position = RX_Message[2] + 1;
-              position_set = true;
-            }
-            if(keyArray[6] >> 3 & B1) { // check if east turned off
-              broadcastEndOfHandshake();
-            }
-            else {
-              turnoffEast();
-              broadcastPosition();
-            }
-          }
-          break;
-        }
-      case 'E':
-        {
-          if(position == 0) {
-            receiver = true;
+          if(keyArray[6] >> 3 & B1) { // check if east turned off
+            broadcastEndOfHandshake();
           }
           else {
-            receiver = false;
+            turnoffEast();
+            broadcastPosition();
           }
-          octave =  position + lowest_octave;
-          break;
         }
-    }
+        xSemaphoreGive(keyArrayMutex);
+        break;
+      }
+    case 'E':
+      {
+        if(position == 0) {
+          receiver = true;
+        }
+        else {
+          receiver = false;
+        }
+        octave =  position + lowest_octave;
+        break;
+      }
   }
 }
+
+std::array<bool, 12> getKeys(uint8_t* keys){
+  std::array<bool, 12> rtn;
+  
+  uint8_t CDs = keys[0];
+  uint8_t EG = keys[1];
+  uint8_t GsB =keys[2];
+  rtn[0] = (~CDs >> 0) & B1;
+  rtn[1] = (~CDs >> 1) & B1;
+  rtn[2] = (~CDs >> 2) & B1;
+  rtn[3] = (~CDs >> 3) & B1;
+  rtn[4] = (~EG  >> 0) & B1;
+  rtn[5] = (~EG  >> 1) & B1;
+  rtn[6] = (~EG  >> 2) & B1;
+  rtn[7] = (~EG  >> 3) & B1;
+  rtn[8] = (~GsB >> 0) & B1;
+  rtn[9] = (~GsB >> 1) & B1;
+  rtn[10] = (~GsB >> 2) & B1;
+  rtn[11] = (~GsB >> 3) & B1;
+
+  return rtn;
+}
+
+void decodeTask(void * pvParameters) {
+  uint8_t local_RX_Message[8] = {0};
+  uint8_t keyArray_1[8] = {15,15,15,15,15,15,15,15}; // keyArray from sender 1 will be stored here
+  uint8_t keyArray_2[8] = {15,15,15,15,15,15,15,15}; // keyArray from sender 2 will be stored here
+  xQueueReceive(msgInQ, local_RX_Message, portMAX_DELAY); 
+  xSemaphoreTake(RX_Message_Mutex, portMAX_DELAY);
+  std::copy(std::begin(local_RX_Message), std::end(local_RX_Message), std::begin(RX_Message));
+  xSemaphoreGive(RX_Message_Mutex);
+
+  switch(RX_Message[0]) {
+    case 'H':
+      {
+        xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
+        setRow(5);
+        delayMicroseconds(2);
+        keyArray[5] = readCols();
+        if(keyArray[5] >> 3 & B1) { // west turned off
+          if(!position_set) {
+            position = RX_Message[2] + 1;
+            position_set = true;
+          }
+          if(keyArray[6] >> 3 & B1) { // check if east turned off
+            broadcastEndOfHandshake();
+          }
+          else {
+            turnoffEast();
+            broadcastPosition();
+          }
+        }
+        xSemaphoreGive(keyArrayMutex);
+        break;
+      }
+    case 'E':
+      {
+        if(position == 0) {
+          receiver = true;
+        }
+        else {
+          receiver = false;
+        }
+        octave =  position + lowest_octave;
+        break;
+      }
+  }
+  
+  // storing received message in correct position
+  switch(RX_Message[4]) {
+    case 1:
+      {
+        xSemaphoreTake(RX_Message_Mutex, portMAX_DELAY);
+        std::copy(std::begin(RX_Message), std::end(RX_Message), std::begin(keyArray_1));
+        xSemaphoreGive(RX_Message_Mutex);
+        break;
+      }
+    case 2:
+      {
+        xSemaphoreTake(RX_Message_Mutex, portMAX_DELAY);
+        std::copy(std::begin(RX_Message), std::end(RX_Message), std::begin(keyArray_2));
+        xSemaphoreGive(RX_Message_Mutex);
+        break;
+      }
+  }
+
+  // now we have keyArray, keyArray_1, keyArray_2
+  // set currentStepSize, currentAverage
+  uint8_t keyArray_0[8] = {0};
+  xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
+  std::copy(std::begin(keyArray), std::end(keyArray), std::begin(keyArray_0));
+  xSemaphoreGive(keyArrayMutex);
+  std::array<bool,36> keysPressed;
+  std::array<bool,12> keysPressed_0 = getKeys(keyArray_0);
+  std::array<bool,12> keysPressed_1 = getKeys(keyArray_1);
+  std::array<bool,12> keysPressed_2 = getKeys(keyArray_2);
+  for(int i = 0; i < 12; i++) {
+    keysPressed[i] = keysPressed_0[i];
+    keysPressed[i+12] = keysPressed_1[i];
+    keysPressed[i+24] = keysPressed_2[i];
+  }
+  uint8_t octaves[3] = {octave, keyArray_1[3], keyArray_2[3]};
+  int32_t localCurrentStepSize[n] = {0};
+  int32_t localCurrentAverage[n] = {0};
+  int j = 0;
+  for(int i = 0; i < n; i++) {
+    while(j < 36) {
+      if(keysPressed[j]) {
+        localCurrentStepSize[i] = stepSizes[j % 12] << (5 - octaves[j / 12]);
+        //localCurrentAverage[i] = averages[j % 12]; // TODO: what is average
+        j++;
+        break;
+      }
+      j++;
+    }
+  }
+  xSemaphoreTake(stepSizeMutex, portMAX_DELAY);
+  std::copy(std::begin(localCurrentStepSize), std::end(localCurrentStepSize), std::begin(currentStepSize));
+  std::copy(std::begin(localCurrentAverage), std::end(localCurrentAverage), std::begin(currentAverage));
+  xSemaphoreGive(stepSizeMutex);
+}
+
+// void decodeTask(void * pvParameters) {
+//   uint8_t local_RX_Message[8] = {0};
+//   bool local_externalKeyPressed;
+//   while (true) {
+//     for (int i = 0; i < 3; i++) {
+//       setRow(i);
+//       delayMicroseconds(2);
+//       keyArray[i] = readCols();
+//     }
+//     // Call function for setting stepsize
+//     if(receiver && !externalKeyPressed)
+//       findKeyWithFunc(&setStepSize);
+//     if(!receiver)
+//       findKeyWithFunc(&setCommMessage);
+
+//     // wait until a message is available in the queue
+//     xQueueReceive(msgInQ, local_RX_Message, portMAX_DELAY);
+
+//     xSemaphoreTake(RX_Message_Mutex, portMAX_DELAY);
+//     std::copy(std::begin(local_RX_Message), std::end(local_RX_Message), std::begin(RX_Message));
+//     xSemaphoreGive(RX_Message_Mutex);
+
+//     switch(RX_Message[0]) {
+//       case 'R':
+//         {
+//           local_externalKeyPressed = false;
+//           __atomic_store_n(&externalKeyPressed,local_externalKeyPressed,__ATOMIC_RELAXED);
+//           notes empty [4]= {None, None, None, None};
+//           setStepSize(empty);
+//           break;
+//         }
+//       case 'P':
+//         {
+//           local_externalKeyPressed = true;
+//           __atomic_store_n(&externalKeyPressed,local_externalKeyPressed,__ATOMIC_RELAXED);
+//           // TODO: Fixing this
+//           int32_t localCurrentStepSize = stepSizes[RX_Message[2]] >> (5 - RX_Message[1]);
+//           xSemaphoreTake(stepSizeMutex, portMAX_DELAY);
+//           for (int i=0; i<n; i++) {
+//             if(currentStepSize[i] == 0) {
+//               currentStepSize[i] = localCurrentStepSize;
+//               break;
+//             };
+//           }
+//           xSemaphoreGive(stepSizeMutex);
+//           break;
+//         }
+//       case 'H':
+//         {
+//           setRow(5);
+//           delayMicroseconds(2);
+//           keyArray[5] = readCols();
+//           if(keyArray[5] >> 3 & B1) { // west turned off
+//             if(!position_set) {
+//               position = RX_Message[2] + 1;
+//               position_set = true;
+//             }
+//             if(keyArray[6] >> 3 & B1) { // check if east turned off
+//               broadcastEndOfHandshake();
+//             }
+//             else {
+//               turnoffEast();
+//               broadcastPosition();
+//             }
+//           }
+//           break;
+//         }
+//       case 'E':
+//         {
+//           if(position == 0) {
+//             receiver = true;
+//           }
+//           else {
+//             receiver = false;
+//           }
+//           octave =  position + lowest_octave;
+//           break;
+//         }
+//     }
+//   }
+// }
 
 void CAN_TX_Task(void * pvParameters) {
   uint8_t msgOut[8];
@@ -621,9 +801,6 @@ void initialCheck() {
   if((keyArray[5] >> 3) & B1) { // west not connected
     position = 0;
     position_set = true;
-    if((keyArray[6] >> 3) & B1) {
-      CAN_Init(true);
-    }
   }
 }
 
@@ -634,6 +811,7 @@ void initialHandshake() {
   if(position_set) { // left most
     if((keyArray[6] >> 3) & B1) { // the only module
       octave = position + lowest_octave;
+      only_module = true;
       // end of handshake
     }
     else {
@@ -732,9 +910,8 @@ void setup() {
     1,/* Task priority*/
     &displayHandle
   );  /* Pointer to store the task handle*/
-
-  //Initialise Decode loop
   TaskHandle_t decodeHandle = NULL;
+  //Initialise Decode loop
   xTaskCreate(
     decodeTask,   /* Function that implements the task */
     "decode",     /* Text name for the task */
@@ -743,6 +920,17 @@ void setup() {
     1,            /* Task priority */
     &decodeHandle /* Pointer to store the task handle */
   );
+
+  // //Initialise handshakeDecode loop
+  // TaskHandle_t handshakeDecodeHandle = NULL;
+  // xTaskCreate(
+  //   handshakeDecodeTask,   /* Function that implements the task */
+  //   "handshakeDecode",     /* Text name for the task */
+  //   256,          /* Stack size in words, not bytes */
+  //   NULL,         /* Parameter passed into the task */
+  //   1,            /* Task priority */
+  //   &handshakeDecodeHandle /* Pointer to store the task handle */
+  // );
 
   //Initialise transmit thread
   TaskHandle_t transmitHandle = NULL;
@@ -770,6 +958,21 @@ void setup() {
   CAN_Start(); // start here as CAN_Init value can change inside initialCheck()
   delayMicroseconds(1000000); // wait 1 sec
   initialHandshake();
+
+  //vTaskSuspend(handshakeDecodeHandle);
+
+  // TaskHandle_t decodeHandle = NULL;
+  // if(true) { // TODO: change this to receiver only
+  //   //Initialise Decode loop
+  //   xTaskCreate(
+  //     decodeTask,   /* Function that implements the task */
+  //     "decode",     /* Text name for the task */
+  //     256,          /* Stack size in words, not bytes */
+  //     NULL,         /* Parameter passed into the task */
+  //     1,            /* Task priority */
+  //     &decodeHandle /* Pointer to store the task handle */
+  //   );
+  // }
 
   vTaskStartScheduler();
 
